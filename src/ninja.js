e1e8256dd319f35073a28ad71853f4a4b4f1dcb8
udeogu ifechuwku <ifebrand6@gmail.com>
2020-01-22 16:06:49 +0100

    added condition to sort ninjas by age

--- src/ninja.js
+++ src/ninja.js
@@ -4,6 +4,7 @@ const Ninjas = ({ninjas}) => {
     // const {ninjas}= props
         
         const ninjaList = ninjas.map(ninja => {
+            if (ninja.age > 20){
             return (
                 <div className="ninja" key={ninja.id}>
                     <div>Name: {ninja.name}</div>
@@ -11,6 +12,18 @@ const Ninjas = ({ninjas}) => {
                     <div>Belt: {ninja.belt}</div>
                 </div>
             )
+        } else {
+            return null
+        }
+
+        //    return ninja.age > 20 ? (
+        //         <div className="ninja" key={ninja.id}>
+        //                      <div>Name: {ninja.name}</div>
+        //                      <div>Age: {ninja.age} </div>
+        //                      <div>Belt: {ninja.belt}</div>
+        //                 </div>  
+        //     ) : null;
+
         })
 
         return (
